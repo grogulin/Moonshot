a009f73c2b40c8e28827ea1270d78dc3a953c49e
Yaroslav Grogul <time7bass@gmail.com>
2023-01-08 18:41:24 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ярослав Грогуль on 08.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
